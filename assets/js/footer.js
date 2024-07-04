def343d27f3644131c7e9be67d8c6f1a3a46b0c8
Your Name <you@example.com>
2024-07-04 13:55:54 -0600

    reworking

--- assets/js/footer.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("/assets/views/footer.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("footer-placeholder").innerHTML = data;
-        });
-});
--- assets/js/menu.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("/assets/views/menu.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("menu-placeholder").innerHTML = data;
-        });
-});
