2b100dbea172561ecaa3a794b9f533bf93170137
Your Name <you@example.com>
2024-07-10 15:17:25 -0600

    changefinal

--- assets/js/mapsvele.js
+++ assets/js/mapsvele.js
@@ -1,11 +1,57 @@
 document.addEventListener('DOMContentLoaded', function () {
     var map = L.map('map').setView([12.121162322854948, -86.30601807402405], 13);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    // Definir capas base
+    var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    });
 
-    L.marker([12.121162322854948, -86.30601807402405]).addTo(map)
-        .bindPopup('Hospital Fernando Vélez Paiz')
+    var satelliteLayer = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.opentopomap.org/copyright">OpenTopoMap</a> contributors'
+    });
+
+    // Añadir capa base por defecto
+    osmLayer.addTo(map);
+
+    // Añadir control de capas
+    var baseMaps = {
+        "OpenStreetMap": osmLayer,
+        "Satellite": satelliteLayer
+    };
+
+    L.control.layers(baseMaps).addTo(map);
+
+    // Añadir marcador
+    var marker = L.marker([12.121162322854948, -86.30601807402405]).addTo(map)
+        .bindPopup('Hospital Fernando Vélez Paiz<br><a href="https://www.google.com/maps/dir/?api=1&destination=12.121162322854948,-86.30601807402405" target="_blank">Ir con Google Maps</a><br><a href="https://waze.com/ul?ll=12.121162322854948,-86.30601807402405&navigate=yes" target="_blank">Ir con Waze</a>')
         .openPopup();
+
+    // Función para reubicar el marcador
+    function resetMarker() {
+        map.setView([12.121162322854948, -86.30601807402405], 13);
+        marker.setLatLng([12.121162322854948, -86.30601807402405]);
+        marker.openPopup();
+    }
+
+    // Añadir evento al botón para reubicar el marcador
+    document.getElementById('reset-marker').addEventListener('click', resetMarker);
+
+    // Mostrar ubicación del usuario
+    map.locate({setView: true, maxZoom: 16});
+
+    function onLocationFound(e) {
+        var radius = e.accuracy / 2;
+
+        L.marker(e.latlng).addTo(map)
+            .bindPopup("Estás dentro de " + radius + " metros desde este punto").openPopup();
+
+        L.circle(e.latlng, radius).addTo(map);
+    }
+
+    function onLocationError(e) {
+        alert(e.message);
+    }
+
+    map.on('locationfound', onLocationFound);
+    map.on('locationerror', onLocationError);
 });
