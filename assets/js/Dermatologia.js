87f1d6537a27ba5d002363cd07d5a4277857c7d9
Your Name <you@example.com>
2024-07-18 16:31:16 -0600

    Se agrega informacion a Dermatologico

--- assets/js/Dermatologia.js
+++ assets/js/Dermatologia.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var hospitalLocation = [12.148108301879251, -86.2768067];
-    var map = L.map('map').setView(hospitalLocation, 13);
+    let hospitalLocation = [12.147682835239348, -86.29887787617419];
+    let map = L.map('map').setView(hospitalLocation, 13);
+
 
     // Definir capas base
-    var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    let osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
 
-    var satelliteLayer = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    let satelliteLayer = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.opentopomap.org/copyright">OpenTopoMap</a> contributors'
     });
 
@@ -15,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
     osmLayer.addTo(map);
 
     // Añadir control de capas
-    var baseMaps = {
+    let baseMaps = {
         "OpenStreetMap": osmLayer,
         "Satellite": satelliteLayer
     };
@@ -23,8 +24,8 @@ document.addEventListener('DOMContentLoaded', function () {
     L.control.layers(baseMaps).addTo(map);
 
     // Añadir marcador del hospital
-    var marker = L.marker(hospitalLocation).addTo(map)
-        .bindPopup('Centro Nacional de Oftalmología<br><a href="https://www.google.com/maps/dir/?api=1&destination=12.121162322854948,-86.30601807402405" target="_blank">Ir con Google Maps</a><br><a href="https://waze.com/ul?ll=12.121162322854948,-86.30601807402405&navigate=yes" target="_blank">Ir con Waze</a>')
+    let marker = L.marker(hospitalLocation).addTo(map)
+        .bindPopup('Centro Nacional de Dermatologia<br><a href="https://www.google.com/maps/dir/?api=1&destination=12.147682835239348,-86.29887787617419" target="_blank">Ir con Google Maps</a><br><a href="https://waze.com/ul?ll=12.147682835239348,-86.29887787617419&navigate=yes" target="_blank">Ir con Waze</a>')
         .openPopup();
 
     // Función para reubicar el marcador del hospital
@@ -41,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function () {
     map.locate({setView: false, maxZoom: 4});
 
     function onLocationFound(e) {
-        var radius = e.accuracy / 2;
+        let radius = e.accuracy / 2;
 
         L.marker(e.latlng).addTo(map)
             .bindPopup("Estás dentro de " + radius + " metros desde este punto").openPopup();
